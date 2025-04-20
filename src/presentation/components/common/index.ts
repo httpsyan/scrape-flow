74ef9652e6d9e62efc2e80c733ba0bd951a0124e
Yan Castro <yancastrohenrique1@gmail.com>
2025-04-20 19:12:02 -0300

    chore: atualiza arquivos de índice dos componentes

--- src/presentation/components/common/index.ts
+++ src/presentation/components/common/index.ts
@@ -2,3 +2,7 @@ export * from "./sidebar";
 export * from "./logo";
 export * from "./breadcrumbHeader";
 export * from "./themeModeToggle";
+export * from "./createWorkFlow";
+export * from "./workflowCard";
+export * from "./tooltipWrapper";
+export * from "./deleteWorkflow";
