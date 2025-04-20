440aecb3d082890d0a9b077752a817d7b4f57a61
Yan Castro <yancastrohenrique1@gmail.com>
2025-04-19 22:28:18 -0300

    feat: implementa configuração do Prisma e estrutura de workflows

---
+++ prisma/migrations/20250419202007_init_db/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "workflows" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "definition" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "workflows_userId_name_key" ON "workflows"("userId", "name");
